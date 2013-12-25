091505adda433884bbd22e6a53689b25c9b2f2cc
Suguru Dobashi <dobashisuguru@gmail.com>
2013-12-25 11:32:54 +0900

    test

--- VS2012Demo/NewClass.cs
+++ VS2012Demo/NewClass.cs
@@ -8,12 +8,6 @@ namespace VS2012Demo
 {
     class NewClass
     {
-        //Changes 
-        //Changes to the NewClass
-        //Changes after saving but before Commit.
-        //Changes to the NewClass
-        //Changes after saving but before Commit.
-        //I don't get the detail about Git yet.
-        //I don't get the detail about Git yet.
+
     }
 }
